5daf6e8776dfb81e7362ef8139f02dadedb8fe74
Обезличенная УЗ для стажировки-направление frontend-разработ. 12 <cr_ovr_fr12@efko.ru>
2025-04-03 21:17:30 +0300

    initial commit

---
+++ Services/WriteAndReadService.cs
@@ -0,0 +1,33 @@
+﻿namespace WritersAndReadersApp.Services
+{
+    public static class WriteAndReadService 
+    {
+        private static int _count = 0;
+        private static readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+        public static void AddToCount(int value)
+        {
+            _lock.EnterWriteLock();
+            try
+            {
+                _count += value;
+            }
+            finally
+            {
+                _lock.ExitWriteLock();
+            }
+        }
+
+        public static int GetCount()
+        {
+            _lock.EnterReadLock();
+            try
+            {
+                return _count;
+            }
+            finally
+            {
+                _lock.ExitReadLock();
+            }
+        }
+    }
+}
